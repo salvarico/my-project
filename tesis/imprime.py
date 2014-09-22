b688ba05202d2b7c81f69e296794ebc47c2969d1
salvarico <salvarico@gmail.com>
2014-09-22 01:40:14 -0500

    Folder de tesis

---
+++ tesis/imprime.py
@@ -0,0 +1,1 @@
+print 'A esto me refiero con no poner todo el codigo en el documento. Este archivo puede estar en el directorio y seria totalmente reproducible el .ipynb mientras tengas el mismo directorio'
